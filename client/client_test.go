c61751c382dcb5458c8d32cb81e9b5953fcaa6ff
bruteforce1414 <bruteforce1414@gmail.com>
2018-11-29 17:14:29 +0500

    Добавила в тест другой сайт

--- client/client_test.go
+++ client/client_test.go
@@ -15,18 +15,18 @@ func TestHttpClient_Get(t *testing.T) {
 	server:= httptest.NewServer(mux)
 	defer server.Close()
 
-	mux.HandleFunc("/a/11526/vyezzhaya-vecherom-na-velosipede-na-dorogu-imejte-s-soboj-komplekt-ispravno-rabotayuschego-sveta-na-vsyu-poezdku", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/teachers/283208/", func(w http.ResponseWriter, r *http.Request) {
 		// а это функция-handler как в сервере и ты уже знаешь как тут писать ответ клиенту
 	})
 
 	// нам нужно передать в функцию создания клиента URL сервера, к которому нужно делать запросы
-	clientTest := client.NewHttpClient("http://kstatida.ru") // незабудь добавить импортированный пакет перед функция создания клиента
+	clientTest := client.NewHttpClient("http://usatu.com") // незабудь добавить импортированный пакет перед функция создания клиента
 
-	repos, err := clientTest.Get("/a/11526/vyezzhaya-vecherom-na-velosipede-na-dorogu-imejte-s-soboj-komplekt-ispravno-rabotayuschego-sveta-na-vsyu-poezdku")
+	repos, err := clientTest.Get("/teachers/283208/")
 	if err!= nil {
 		t.Fatal(err)
 	}
-	a.Equal(len(repos),43565)
+	a.Equal(len(repos),13555)
 
 
 }
